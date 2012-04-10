43634639a0f37cbe8a933aa2a29312743d34190d
Trevor Elliott <trevor@hellokitty.(none)>
2012-04-09 21:42:05 -0700

    Remove persistent, and switch to using the sqlite bindings

--- src/Main.hs
+++ src/Main.hs
@@ -1,22 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Storage.Query
-import Storage.Schema (migrateAll)
 import UI (playerInterface,context)
 import Tag
 
-import Control.Monad.Trans (liftIO)
-import Database.Persist
-import Database.Persist.Sqlite
-import Database.Persist.TH
 import Graphics.Vty.Widgets.All (runUi,newCollection)
 
 
 main :: IO ()
-main  = withSqliteConn ":memory:" $ runSqlConn $ do
-  runMigration migrateAll
-  liftIO $ do
-    c       <- newCollection
-    _mainUi <- playerInterface c
-    runUi c context
+main  = do
+  c       <- newCollection
+  _mainUi <- playerInterface c
+  runUi c context
